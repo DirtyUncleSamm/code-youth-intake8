76ac23b4c57f23124c26a4bb3a5644be3862c70e
Sammantha Preston <94912168+SammPreston@users.noreply.github.com>
2022-11-24 11:45:33 -0500

    Link fix
    in footer link color changed to blue to make them easier to find

---
+++ Main-Course/Javascript/javaScript/operators.js
@@ -0,0 +1,20 @@
+let  a= 20, b= 10, d= 4;
+//c= a+b;
+//c= a-b;
+//c= a*b;
+//c= a/b;
+let c= b/d;
+let x= b%d;
+console.log("original value of a:",a);
+console.log("original value of b:",b);
+console.log("original value of d:",d);
+console.log(c);
+console.log(x);
+ d++;//increment operator
+console.log("this displays incremental value d=d+1:",d);
+b--;
+console.log("value of b= b-1:",b);
+console.log("exponent of b to the power of 3",b**3);//exponentiation operator
+
+ a *= 5; //a= a*5;
+console.log(a);
---
+++ Main-Course/Javascript/javaScript/print.js
@@ -0,0 +1,30 @@
+console.log("This is my first JavaScript document. This is an externally linked file!!");
+let number;
+number=5;
+let a = 10;
+console.log("To display a value",a,number);
+let b = 20;
+let c = a + b;
+console.log("the sum of a + b:",c);
+let d = a * c;
+console.log("the product of a * c",d);
+let e = 10;
+let f = 2;
+let g = 10 * 2;
+console.log("the product of 10 and 2 is: ", g);
+
+//int a=800;
+//float b=30.56;
+//char  c='a';
+
+let name1 = "Ada Lovelace";
+console.log("typeof will display the datatype of a variable",typeof name1);//display datatypes
+let name2 = `Sammantha`;//backticks string
+console.log(name2);
+let check = true;
+console.log("another typeof to display datatype:",typeof check);
+let student = {"name":"john"};//creating an object
+console.log("student:", student);
+console.log(typeof student);
+
+//operators
