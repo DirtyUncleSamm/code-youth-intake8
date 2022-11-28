d77861cdb9daed79224ade608cc4f4fea075d3e0
Sammantha Preston <94912168+SammPreston@users.noreply.github.com>
2022-11-28 11:42:59 -0500

    update

--- Main-Course/Javascript/javaScript/loops.js
+++ Main-Course/Javascript/javaScript/loops.js
@@ -2,13 +2,22 @@
 //     code that needs to be repeated
 // }standard name for initialization for loops is i, j, k
 //flow chart is an algorithm in pictorial form
+
 // for(let i=3; i<=20;i++){
 //     console.log(i);
 // } this will print numbers 1-20 in the console.log
 
 // for(let i =2; i<=50; i+=2){
 //     console.log(i);
-// }
-for(let i=20; i>0;i--){
-    console.log(i);
-}
+// }this code displays even numbers
+
+// for(let i=20; i>0;i--){
+//     console.log(i);
+// }this code displays numbers from 20 to 1
+
+
+// let sum = 0;
+// for(let i=1; i<=10;i++){
+//     sum = sum+i;
+//     console.log(sum);
+// } this code displays the sum of the proceeding numbers i.e. fibonocci sequence
