c76e8af40bf68575fcc563b7e225721ec7444275
Sammantha Preston <94912168+SammPreston@users.noreply.github.com>
2022-11-28 14:04:23 -0500

    Update loops.js

--- Main-Course/Javascript/javaScript/loops.js
+++ Main-Course/Javascript/javaScript/loops.js
@@ -21,3 +21,31 @@
 //     sum = sum+i;
 //     console.log(sum);
 // } this code displays the sum of the proceeding numbers i.e. fibonocci sequence
+
+
+//---------popMilk--------------
+// let num = prompt("write an num");
+//     if (num % 3 == 0 && num% !5 == 0){
+//         console.log("milk");
+//     }else if (num % 5 == 0 && num% !3 == 0){
+//         console.log("pop");
+//     }else if (num%3 == 0 && num%5 == 0){
+//         console.log("popMilk")
+//     }else{console.log("not divisible by three or five")}
+
+
+
+
+
+
+let sum = 0;
+for(let i=1; i<=10;i++){
+    num=prompt("enter prompt");
+    sum = sum +num;
+    
+}
+console.log("sum of 10 numbers", sum);
+
+
+
+
