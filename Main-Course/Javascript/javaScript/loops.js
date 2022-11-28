e94301b62fa11385033007f373d84e2e1f41f478
Sammantha Preston <94912168+SammPreston@users.noreply.github.com>
2022-11-28 11:06:02 -0500

    for loops decrease

--- Main-Course/Javascript/javaScript/loops.js
+++ Main-Course/Javascript/javaScript/loops.js
@@ -2,6 +2,13 @@
 //     code that needs to be repeated
 // }standard name for initialization for loops is i, j, k
 //flow chart is an algorithm in pictorial form
-for(let i=3; i<=20;i++ ){
+// for(let i=3; i<=20;i++){
+//     console.log(i);
+// } this will print numbers 1-20 in the console.log
+
+// for(let i =2; i<=50; i+=2){
+//     console.log(i);
+// }
+for(let i=20; i>0;i--){
     console.log(i);
 }
