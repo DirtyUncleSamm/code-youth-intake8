e88aa3a09458bdb44012f6121204ec44bae294f4
Sammantha Preston <94912168+SammPreston@users.noreply.github.com>
2022-12-14 12:51:44 -0500

    light/dark
    I am having an issue running external script in both this project and my launch simulation

--- DOM/DomChallenges/js/darkLight.js
+++ DOM/DomChallenges/js/darkLight.js
@@ -1,5 +1,16 @@
-const h1 = document.getElementsByTagName('h1');
-const dm = document.getElementById('darkMode');
-dm.onclick = function(){
-    h1.innerText="This is dark mode";
+
+function dm(){
+    const h1 = document.getElementById('title');
+    h1.innerHTML="This is dark mode"
+    
+    let darkDiv = document.getElementById('change');
+    console.log(darkDiv);
+    darkDiv.setAttribute('className', 'darkdiv');
+    
+}
+function lm(){
+    const h1 = document.getElementById('title');
+    h1.innerHTML="This is light mode";
+
+    
 }
