e2c4779e85b5d75eae87eeef77e8ac361a37652d
Sammantha Preston <94912168+SammPreston@users.noreply.github.com>
2022-12-14 10:21:00 -0500

    JavaScript review

--- DOM/DomChallenges/js/shaceship.js
+++ DOM/DomChallenges/js/shaceship.js
@@ -1,5 +1,25 @@
-const takeOff = document.createElement("button");
-takeOff.innerHTML="Take Off!";
-
-const mc = document.getElementById('missionControl')
-mc.appendChild(takeOff);
+  function launch(){
+            const launchRocket = document.getElementById('launchStatus');
+            launchRocket.innerHTML="The shuttle has been launched";
+        }
+        function warning(ele){
+            const missionAbort = document.getElementById('missionControl');
+            missionAbort.style.backgroundColor="red";
+        }
+        function warningAbort(elem){
+            const revertMissionAbort = document.getElementById('missionControl');
+            revertMissionAbort.style.backgroundColor="";
+            
+        }
+        function launchWarning(){
+            const warningBox = document.createElement("div");
+            missionControl.appendChild(warningBox);
+            warningBox.className='box';
+            warningBox.style.cssText
+            warningBox.setAttribute
+            const abortCheck = document.createElement('p');
+            abortCheck.innerHTML="Are you sure you want to abort mission?";
+            warningBox.appendChild('abortCheck')
+        }
+        
+        const control2 = document.getElementById('abort');
