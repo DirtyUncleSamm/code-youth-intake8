14ab2a75d29062f1023b81041973a0d3c72f2d5b
Sammantha Preston <94912168+SammPreston@users.noreply.github.com>
2022-12-15 08:47:42 -0500

    added styles

--- DOM/DomChallenges/js/shaceship.js
+++ DOM/DomChallenges/js/shaceship.js
@@ -1,30 +1,25 @@
-  function launch(){
-            const launchRocket = document.getElementById('launchStatus');
-            launchRocket.innerHTML="The shuttle has been launched";
+        const dbclc = document.getElementById('takeoff');
+        dbclc.ondblclick = function(){
+            const divr = document.createElement('div');
+            divr.className="missionFailed"
+
+        }
+        function warning(){
+            const missionAbort = document.getElementById('missionControl');
+            missionAbort.style.backgroundColor="red";
+        }
+        function warningAbort(){
+            const revertMissionAbort = document.getElementById('missionControl');
+            revertMissionAbort.style.backgroundColor="";
+            
+        }
+        function launchWarning(ele){
+            const warningBox = document.createElement("div");
+            missionControl.appendChild(warningBox);
+            warningBox.className='box';
+            const abortCheck = document.createElement('p');
+            abortCheck.innerHTML="Are you sure you want to abort mission?";
+            warningBox.appendChild('abortCheck')
         }
         
-        // const dbclc = document.getElementById('takeoff');
-        // dbclc.ondblclick = function(){
-        //     const divr = document.createElement('div');
-        //     divr.className="missionFailed"
-
-        // }
-        // function warning(ele){
-        //     const missionAbort = document.getElementById('missionControl');
-        //     missionAbort.style.backgroundColor="red";
-        // }
-        // function warningAbort(elem){
-        //     const revertMissionAbort = document.getElementById('missionControl');
-        //     revertMissionAbort.style.backgroundColor="";
-            
-        // }
-        // function launchWarning(ele){
-        //     const warningBox = document.createElement("div");
-        //     missionControl.appendChild(warningBox);
-        //     warningBox.className='box';
-        //     const abortCheck = document.createElement('p');
-        //     abortCheck.innerHTML="Are you sure you want to abort mission?";
-        //     warningBox.appendChild('abortCheck')
-        // }
-        
-        // const control2 = document.getElementById('abort');
+        const control2 = document.getElementById('abort');
