4851210bfdbaf6dfe3347693bd47c3cd54696ed5
Sammantha Preston <94912168+SammPreston@users.noreply.github.com>
2023-01-12 19:33:34 -0500

    Bug fixes on exprSurvey

--- exprSurvay/db.js
+++ exprSurvay/db.js
@@ -6,7 +6,53 @@ const pool = new Pool({
     password: 'asdf',
     port: 5432,
 })
+// -- products Table Queries
 
+// -- display all products in .json format
+const getProducts = (req,res)=>{
+    pool.query('SELECT * FROM products', (err, results)=>{
+        if(err){
+            throw err;
+        }
+        return res.json(results.rows);
+    })
+}
+
+// -- displays specific product by PRIMARY KEY 'id'
+const getProductsById = (req, res) => {
+    const id = parseInt(req.params.id);
+    pool.query('SELECT * FROM products where id=$1', [id], (err, results)=>{
+        if(err){
+            throw err;
+        }
+        // res.render("products", {data:results.rows})
+        return res.json(results.rows);
+    })
+}
+
+const createProduct = (req, res) => {
+    const name = req.body.name;
+    console.log(name);
+    const category = req.body.category;
+    const proFresh = req.body.fresh;
+    const photo = req.body.photo;
+    const description = req.body.description;
+    const price = req.body.price;
+
+    pool.query('INSERT INTO products(name, category, product_freshness, photo, description, price) VALUES($1, $2,$3,$4,$5,$6)', [name, category, proFresh, photo, description, price], (err, results)=>{
+        if(err){
+            throw err;
+        }
+        res.send("new product added")
+        // res.send("new product", "Name:" $:(name)) -- displays new product information 
+    })
+}
+
+
+
+// -- users Table Queries
+
+// -- select and display users as a .json file from psql
 const getUsers = (req,res)=>{
     pool.query('SELECT * FROM users', (err, results)=>{
         if(err){
@@ -16,4 +62,55 @@ const getUsers = (req,res)=>{
     })
 }
 
-module.exports = getUsers;
+// -- Select users by their PRIMARY KEY 'id'
+const getUsersById = (req, res) => {
+    const id = parseInt(req.params.id);
+    pool.query('SELECT * FROM users where id=$1', [id], (err, results)=>{
+        if(err){
+            throw err;
+        }
+        // res.render("users", {data:results.rows})
+        return res.json(results.rows);
+    })
+}
+
+// --  This is a duplicate funtion of the one above except it displays the proper syntax for adding multiple parameters for search
+
+// const getUsersById = (req, res) => {
+//     const id = NUMBER(re.params.id);
+//     pool.query('SELECT * FROM users WHERE id=$1 AND name=$2', [id, name], (err, results)=>{
+//         if(err){
+//             throw err;
+//         }
+//         return res.json(results.rows);
+//     })
+// }
+
+// -- SAVE user input from browser
+const createUser = (req, res) => {
+    const name = req.body.name;
+    console.log(name);
+    const email = req.body.email;
+
+    pool.query('INSERT INTO users(name, email) VALUES($1,$2)', [name,email], (err, results)=>{
+        if(err){
+            throw err;
+        }
+        res.send("new user added")
+        // res.send("new user", "Name:" $:(name))
+    })
+}
+// -- This did not work for Raninder
+
+// const deleteUser = (req, res) => {
+//     const id = parseInt(req.params.id);
+
+//     pool.query('DELETE FROM users WHERE id=$1',[id], (err,res)=>{
+//         if(err){
+//             throw err;
+//         }
+//         res.send('user deleted with id: ${id}')
+//     })
+// }
+
+module.exports = {getUsers, getUsersById, createUser, getProducts, getProductsById, createProduct};
--- exprSurvay/index.js
+++ exprSurvay/index.js
@@ -1,13 +1,39 @@
 const express = require('express')
+const { Pool } = require('pg')
 const app = express()
-const getUsers = require('./db.js')
+const {getUsers, getUsersById, createUser, getProducts, getProductsById, createProduct} = require('./db.js')
 const port = 3001
-
+app.use(express.urlencoded());
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + '/public'));
 
-app.get('/users', getUsers);
+// -- Products Table
+app.get('/products', getProducts);
+app.get('products/:id', getProductsById);
+app.post('/save', createProduct);
 
+
+
+// -- Users Table
+
+app.get('/users', getUsers);
+app.get('/users/:id', getUsersById);
+app.post('/save1', createUser);
+
+// -- delete user by id -- Delete may not be possible from the browser through psql
+//app.delete('/users/:id', deleteUser);
+
+// -- to encode data pulled by psql
+
+
+// app.post('/save', (req, res)=>{
+// // res.send("form submitted")
+// const name = req.body.name;
+// const email = req.body.email;
+// res.send(`Name: ${name} Email: ${email}`);
+// })
+
+// -- Navigation 'end points' to render .ejs pages
 app.get('/', (req,res)=>{
     res.render("home")
 })
@@ -17,14 +43,9 @@ app.get('/survay', (req,res)=>{
 app.get('/form', (req,res)=>{
     res.render("form")
 })
-app.use(express.urlencoded());
-app.post('/save', (req, res)=>{
-// res.send("form submitted")
-const name = req.body.name;
-const email = req.body.email;
-res.send(`Name: ${name} Email: ${email}`);
+app.get('/productForm', (req,res)=>{
+    res.render("productForm")
 })
-
 
 
 app.listen(port, ()=>
