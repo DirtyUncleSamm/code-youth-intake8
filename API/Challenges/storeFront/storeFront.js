a015821e29232251bd7bc7eefbf60f59cff0addb
Sammantha Preston <94912168+SammPreston@users.noreply.github.com>
2023-04-18 18:05:54 -0400

    DOM-Practice

--- API/Challenges/storeFront/storeFront.js
+++ API/Challenges/storeFront/storeFront.js
@@ -1,19 +1,59 @@
 const cartItems = [];
 function addtocart(ele){
     const parent = ele.closest('.section');
-    const price = parent.querySelectorAll('price').innerText;
-    // const priceStr = priceEle.innerText;
-    // const price = parseFloat(priceStr.replace(/[^0-9.-]+/g,""));
+    const price = parseFloat(parent.querySelector('.price').innerText);
     console.log(price);
+    const title = parent.querySelector('.title').innerText;
+    
+    const existingItem = cartItems.find(item => item.title === title);
+    if (existingItem) {
+        existingItem.quantity++;
+        existingItem.total = existingItem.quantity * existingItem.price;
+    } else {
+        const item = {
+            title: title,
+            price: price,
+            quantity: 1,
+            total: price
+        };
+        cartItems.push(item);
+    }
+    
+    let cartTable = document.querySelector('#cart-table');
+    cartTable.innerHTML = '';
+    cartItems.forEach(item => {
+        let row = `<tr>
+            <td>${item.title}</td>
+            <td>${item.price}</td>
+            <td>${item.quantity}</td>
+            <td>${item.total}</td>
+        </tr>`;
+        cartTable.innerHTML += row;
+    });
+}
+
+
+
+
+
+
+
+// const cartItems = [];
+// function addtocart(ele){
+//     const parent = ele.closest('.section');
+//     const price = parent.querySelectorAll('price').innerText;
+//     // const priceStr = priceEle.innerText;
+//     // const price = parseFloat(priceStr.replace(/[^0-9.-]+/g,""));
+//     console.log(price);
     
    
-    const title = parent.querySelector('.title').innerText;
-    let cash = "";
-    cash+= `<tr>`;
-    cash+= `<td>${title}</td>`
-    cash+= `<td class="cost">${price}</td>`;
-    cash+= `</tr>`;
-    // console.log(cash);
-    table.innerHTML += cash;
-}
+//     const title = parent.querySelector('.title').innerText;
+//     let cash = "";
+//     cash+= `<tr>`;
+//     cash+= `<td>${title}</td>`
+//     cash+= `<td class="cost">${price}</td>`;
+//     cash+= `</tr>`;
+//     // console.log(cash);
+//     table.innerHTML += cash;
+// }
 
--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 Code Youth 
-Project Based 
-Web Development Basics
+Project: Cohort Progress Tracker
 
-Tips to get the most out of code youth
-- Take time to write your code, but share your screen when you are stuck
