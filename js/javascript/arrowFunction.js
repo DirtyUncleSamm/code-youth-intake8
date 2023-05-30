5565cbe8a4ee43ae52619b4c6c128b54e26e3d02
AliAfrah <aliafrah2@gmail.com>
2023-05-30 10:11:28 -0400

    Slight changes to JS Exercises

--- js/javascript/arrowFunction.js
+++ js/javascript/arrowFunction.js
@@ -5,23 +5,23 @@ const multiply = (val1, val2) => {
 console.log(multiply(3,6));
 705-919-6357 -- Raj Kumar
 
-// //-- displays area with two lines of code
-// const c = (r) => {
-//     let area = r*r*3.142;
-//     return area;
-// }
-// console.log(c(5));
+//-- displays area with two lines of code
+const c = (r) => {
+    let area = r*r*3.142;
+    return area;
+}
+console.log(c(5));
 
-// // -- Displays area with one line in arrow function
-// const area = (radius) => {return 3.142*radius*radius};
-// console.log("area",area(4))
+// -- Displays area with one line in arrow function
+const area = (radius) => {return 3.142*radius*radius};
+console.log("area",area(4))
 
-// // -- displays name
-// let display = () => {console.log("Alex");}
-// display();
-// // -- displays name with string
-// let displayName= (name) => console.log(`hi ${name}`);
-// displayName('John');
+// -- displays name
+let display = () => {console.log("Alex");}
+display();
+// -- displays name with string
+let displayName= (name) => console.log(`hi ${name}`);
+displayName('John');
 
 // // -- Function that adds a and b
 // let add = (a,b) => {return a+b;}
